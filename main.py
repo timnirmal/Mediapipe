6851fc222d8ac5f80d973a960b855c7c9ad162ea
Thimira Nirmal <timnirmal@gmail.com>
2022-01-23 13:55:23 +0530

    Access Each LandMark of Each Hand

--- main.py
+++ main.py
@@ -14,22 +14,28 @@ while True:
     ret, img = cap.read()
     rgbImg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(rgbImg) # results is a list of Hand objects
-    print(results.multi_hand_landmarks) # This will show the landmarks of all hands
+    #print(results.multi_hand_landmarks) # This will show the landmarks of all hands
 
     if results.multi_hand_landmarks: # if results is not empty
         for hand in results.multi_hand_landmarks: # For each hand in result
+            for id, lm in enumerate(hand.landmark): # For each landmark in hand
+                # print(id, lm) # Print the landmark id and its coordinates
+                h , w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx, cy)   # Print the landmark id and its coordinates
+                if id == 8:
+                    cv2.circle(img, (cx, cy), 15, (0, 0, 255), -1)
             mpDraw.draw_landmarks(img, hand, mpHands.HAND_CONNECTIONS) # Draw the landmarks
 
 
 
     if not ret:
         break
-    # flip the frame horizontally
-    img = cv2.flip(img, 1)
-    # increase frame size
-    img = cv2.resize(img, (640, 480))
-    cv2.imshow('frame', img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    img = cv2.flip(img, 1)    # Flip the frame horizontally
+    img = cv2.resize(img, (640, 480))   # increase frame size
+    cv2.imshow('frame', img)    # Show the frame
+
+    if cv2.waitKey(1) & 0xFF == ord('q'): # Press q to quit
         break
 
 cap.release()
