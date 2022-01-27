24107e55f87b1ff98eb8a5676f44ec5384060044
timnirmal <timnirmal@gmail.com>
2022-01-27 18:51:09 +0530

    Back to Prev Method (Other method take x10 time)

--- PlotHand.py
+++ PlotHand.py
@@ -35,7 +35,7 @@ def CalcLandMarks():
             mpDraw.draw_landmarks(img, hand, mpHands.HAND_CONNECTIONS)  # Draw the landmarks
             all_hands.append(new_hand)  # Append the new_hand to all_hands
 
-            print("Hand : ", new_hand)
+            #print("Hand : ", new_hand)
 
     img = cv2.flip(img, 1)  # Flip the frame horizontally
     img = cv2.resize(img, (640, 480))  # increase frame size
@@ -55,53 +55,81 @@ if __name__ == '__main__':
     hands = mpHands.Hands()
     mpDraw = mediapipe.solutions.drawing_utils  # For drawing landmarks
 
+    # calculate time taken fo below process
+    t = time.time()
+
     while True:
         all_hands = CalcLandMarks()
-        print("New calculation\n\n")
+        #print("New calculation\n\n")
         i = 0
         # calculate time taken fo below process
+        time_avg = 0
+        count = 0
         t = time.time()
 
         for new_hand in all_hands:
             for hand_mk in new_hand:
-                print("hand_mk",new_hand[4])
+                #print("hand_mk",new_hand[4])
                 plt.plot(hand_mk[1], hand_mk[2], 'ro')
                 # draw line between 3 and 4, 3 and 2, 2 and 1, 1 and 0 with color (0, 255, 255)
-                plt.plot([new_hand[3][1], new_hand[4][1]], [new_hand[3][2], new_hand[4][2]], 'c')
-                plt.plot([new_hand[2][1], new_hand[3][1]], [new_hand[2][2], new_hand[3][2]], 'c')
-                plt.plot([new_hand[1][1], new_hand[2][1]], [new_hand[1][2], new_hand[2][2]], 'c')
-                plt.plot([new_hand[0][1], new_hand[1][1]], [new_hand[0][2], new_hand[1][2]], 'c')
+                if hand_mk[0] == 3:
+                    plt.plot([hand_mk[1], new_hand[4][1]], [hand_mk[2], new_hand[4][2]], 'c')
+                if hand_mk[0] == 2:
+                    plt.plot([hand_mk[1], new_hand[3][1]], [hand_mk[2], new_hand[3][2]], 'c')
+                if hand_mk[0] == 1:
+                    plt.plot([hand_mk[1], new_hand[2][1]], [hand_mk[2], new_hand[2][2]], 'c')
+                if hand_mk[0] == 0:
+                    plt.plot([hand_mk[1], new_hand[1][1]], [hand_mk[2], new_hand[1][2]], 'c')
 
-                # draw line between 8 and 7, 7 and 6, 6 and 5, 5 and 0 with color red
-                plt.plot([new_hand[7][1], new_hand[8][1]], [new_hand[7][2], new_hand[8][2]], 'r')
-                plt.plot([new_hand[6][1], new_hand[7][1]], [new_hand[6][2], new_hand[7][2]], 'r')
-                plt.plot([new_hand[5][1], new_hand[6][1]], [new_hand[5][2], new_hand[6][2]], 'r')
-                plt.plot([new_hand[0][1], new_hand[5][1]], [new_hand[0][2], new_hand[5][2]], 'r')
+                    # draw line between 8 and 7, 7 and 6, 6 and 5, 5 and 0 with color red
+                if hand_mk[0] == 7:
+                    plt.plot([hand_mk[1], new_hand[8][1]], [hand_mk[2], new_hand[8][2]], 'r')
+                if hand_mk[0] == 6:
+                    plt.plot([hand_mk[1], new_hand[7][1]], [hand_mk[2], new_hand[7][2]], 'r')
+                if hand_mk[0] == 5:
+                    plt.plot([hand_mk[1], new_hand[6][1]], [hand_mk[2], new_hand[6][2]], 'r')
+                if hand_mk[0] == 0:
+                    plt.plot([hand_mk[1], new_hand[5][1]], [hand_mk[2], new_hand[5][2]], 'r')
 
-    
-                # draw line between 12 and 11, 11 and 10, 10 and 9 with color blue
-                plt.plot([new_hand[11][1], new_hand[12][1]], [new_hand[11][2], new_hand[12][2]], 'b')
-                plt.plot([new_hand[10][1], new_hand[11][1]], [new_hand[10][2], new_hand[11][2]], 'b')
-                plt.plot([new_hand[9][1], new_hand[10][1]], [new_hand[9][2], new_hand[10][2]], 'b')
-                plt.plot([new_hand[0][1], new_hand[9][1]], [new_hand[0][2], new_hand[9][2]], 'b')
-    
-                # draw line between 16 and 15, 15 and 14, 14 and 13 with color green
-                plt.plot([new_hand[15][1], new_hand[16][1]], [new_hand[15][2], new_hand[16][2]], 'g')
-                plt.plot([new_hand[14][1], new_hand[15][1]], [new_hand[14][2], new_hand[15][2]], 'g')
-                plt.plot([new_hand[13][1], new_hand[14][1]], [new_hand[13][2], new_hand[14][2]], 'g')
-                plt.plot([new_hand[0][1], new_hand[13][1]], [new_hand[0][2], new_hand[13][2]], 'g')
-    
-                # draw line between 20 and 19, 19 and 18, 18 and 17, 17 and 0 with color yellow
-                plt.plot([new_hand[19][1], new_hand[20][1]], [new_hand[19][2], new_hand[20][2]], 'y')
-                plt.plot([new_hand[18][1], new_hand[19][1]], [new_hand[18][2], new_hand[19][2]], 'y')
-                plt.plot([new_hand[17][1], new_hand[18][1]], [new_hand[17][2], new_hand[18][2]], 'y')
-                plt.plot([new_hand[0][1], new_hand[17][1]], [new_hand[0][2], new_hand[17][2]], 'y')
+                    # draw line between 12 and 11, 11 and 10, 10 and 9 with color blue
+                if hand_mk[0] == 11:
+                    plt.plot([hand_mk[1], new_hand[12][1]], [hand_mk[2], new_hand[12][2]], 'b')
+                if hand_mk[0] == 10:
+                    plt.plot([hand_mk[1], new_hand[11][1]], [hand_mk[2], new_hand[11][2]], 'b')
+                if hand_mk[0] == 9:
+                    plt.plot([hand_mk[1], new_hand[10][1]], [hand_mk[2], new_hand[10][2]], 'b')
+                if hand_mk[0] == 0:
+                    plt.plot([hand_mk[1], new_hand[9][1]], [hand_mk[2], new_hand[9][2]], 'b')
+
+                    # draw line between 16 and 15, 15 and 14, 14 and 13 with color green
+                if hand_mk[0] == 15:
+                    plt.plot([hand_mk[1], new_hand[16][1]], [hand_mk[2], new_hand[16][2]], 'g')
+                if hand_mk[0] == 14:
+                    plt.plot([hand_mk[1], new_hand[15][1]], [hand_mk[2], new_hand[15][2]], 'g')
+                if hand_mk[0] == 13:
+                    plt.plot([hand_mk[1], new_hand[14][1]], [hand_mk[2], new_hand[14][2]], 'g')
+                if hand_mk[0] == 0:
+                    plt.plot([hand_mk[1], new_hand[13][1]], [hand_mk[2], new_hand[13][2]], 'g')
+
+                    # draw line between 20 and 19, 19 and 18, 18 and 17, 17 and 0 with color yellow
+                if hand_mk[0] == 19:
+                    plt.plot([hand_mk[1], new_hand[20][1]], [hand_mk[2], new_hand[20][2]], 'y')
+                if hand_mk[0] == 18:
+                    plt.plot([hand_mk[1], new_hand[19][1]], [hand_mk[2], new_hand[19][2]], 'y')
+                if hand_mk[0] == 17:
+                    plt.plot([hand_mk[1], new_hand[18][1]], [hand_mk[2], new_hand[18][2]], 'y')
+                if hand_mk[0] == 0:
+                    plt.plot([hand_mk[1], new_hand[17][1]], [hand_mk[2], new_hand[17][2]], 'y')
 
 
-                print("Done")
+                #print("Done")
                 i += 1
 
-        print("Time taken : ", time.time() - t)
+        time_diff = time.time() - t
+        count += 1
+        time_avg += time_diff/count
+        print("Time taken : ", time_diff)
+        print("Average time taken : ", time_avg)
 
         plt.show(block=False)
         plt.pause(0.001)
